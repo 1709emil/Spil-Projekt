60771688169c62952421f14cc834afa9e8c9e55b
Rose <rosefed567@gmail.com>
2021-11-05 09:30:04 +0100

    imgane

--- Main.js
+++ Main.js
@@ -1,1 +1,15 @@
 console.log("dette er main js-filen");
+
+let img = createImage(66, 66);
+
+img.loadPixels();
+for (let i = 0; i < img.width; i++) {
+
+  for (let j = 0; j < img.height; j++) {
+
+    img.set(i, j, color(0, 90, 102));
+  }
+  
+}
+img.updatePixels();
+image(img, 17, 17);
