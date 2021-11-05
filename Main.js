036e33b3c386b194f953f4f8db3da2ef988dada6
Emil Kræmmer Jørgensen <1709emil@gmail.com>
2021-11-05 10:30:41 +0100

    rose is a dumbass

--- Main.js
+++ Main.js
@@ -3,15 +3,4 @@ console.log(window.innerHeight);
 function setup(){
   let canvas = createCanvas(window.innerWidth-18,window.innerHeight-18);
   background('black');
-
-<<<<<<< HEAD
 }
-=======
-function setup() {
-    createCanvas(100, 50);
-    background(153);
-    line(0, 0, width, height);
-  }
-
-//test
->>>>>>> 86270e77689c8c951c85112c20b7ce39d134230b
