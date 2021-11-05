86270e77689c8c951c85112c20b7ce39d134230b
Rose <rosefed567@gmail.com>
2021-11-05 10:23:35 +0100

    tesst

--- Main.js
+++ Main.js
@@ -1,17 +1,9 @@
 console.log("dette er main js-filen");
 
-let img = createImage(66, 66);
-
-img.loadPixels();
-for (let i = 0; i < img.width; i++) {
-
-  for (let j = 0; j < img.height; j++) {
-
-    img.set(i, j, color(0, 90, 102));
+function setup() {
+    createCanvas(100, 50);
+    background(153);
+    line(0, 0, width, height);
   }
-
-}
-img.updatePixels();
-image(img, 17, 17);
 
 //test
