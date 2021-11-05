6cd75cf37985d44627cd7ca92ba139d49281305c
Emil Kræmmer Jørgensen <1709emil@gmail.com>
2021-11-05 10:27:03 +0100

    Canvas

--- Main.js
+++ Main.js
@@ -1,17 +1,7 @@
 console.log("dette er main js-filen");
-
-let img = createImage(66, 66);
-
-img.loadPixels();
-for (let i = 0; i < img.width; i++) {
-
-  for (let j = 0; j < img.height; j++) {
-
-    img.set(i, j, color(0, 90, 102));
-  }
+console.log(window.innerHeight);
+function setup(){
+  let canvas = createCanvas(window.innerWidth-18,window.innerHeight-18);
+  background('black');
 
 }
-img.updatePixels();
-image(img, 17, 17);
-
-//test
