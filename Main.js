cf0dd9943ec40f13c6a855337507a7916f64bd68
Emil Kræmmer Jørgensen <1709emil@gmail.com>
2021-11-12 14:09:26 +0100

    playbutton

--- Main.js
+++ Main.js
@@ -4,7 +4,8 @@ let imgXpos=150;
 let imgYpos=300;
 let imgSize=150;
 let isGameInProgress=false;
-
+let PlayButton;
+let end =0;
 // en funktion der loader alle billederne 
 function preload() {
   for(let i=0; i <animalSprits.length; i ++){
@@ -15,12 +16,23 @@ function preload() {
 // opsætter spillet og laver billderne 
 function setup(){
   let canvas = createCanvas(window.innerWidth-18,window.innerHeight-18);
-  background('black');
-  if(isGameInProgress = true){
-for(let n=0; n <animalSprits.length; n++){
-   image(animalSprits[n],imgXpos,imgYpos,imgSize);
-   imgXpos += 200;}
- }
-
-  
+  background('lightgrey');
+  PlayButton = createImg('/Images/playIcon.png.crdownload',"");
+  PlayButton.position(window.innerWidth/2,window.innerHeight/2);
+  PlayButton.mousePressed(drawImages(true));
+  PlayButton.size(50,50);
 };
+function drawImages(inp) {
+  if(isGameInProgress = inp){
+    for(let n=0; n <animalSprits.length; n++){
+       if(end < 3){
+       image(animalSprits[n],imgXpos,imgYpos,imgSize);
+       imgXpos += 200;}
+       end ++}
+     };
+};
+
+// function draw(){
+//   drawImages();
+  
+// }
