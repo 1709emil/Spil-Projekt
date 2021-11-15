c390ca012a1f24fe3af3bb1195b7ecb4b1c90c18
Emil Kræmmer Jørgensen <1709emil@gmail.com>
2021-11-15 12:46:15 +0100

    playbutton works, you can now click on the images

---
+++ ImageClasses.js
@@ -0,0 +1,10 @@
+class ImageClass{
+ constructor(img,imgXpos,imgYpos,ImgSize,imgId){
+this.Sprites=img;
+this.Xpos = imgXpos;
+this.Ypos = imgYpos;
+this.size = ImgSize;
+this.id=imgId;
+ };
+
+}
--- Main.js
+++ Main.js
@@ -1,11 +1,18 @@
+
+let cow =new ImageClass('Images/cow.jfif',(window.innerWidth/3.7),(window.innerHeight/2),200,1)
+let pig =new ImageClass('Images/gris.jfif',undefined,undefined,200,2)
+let sheep =new ImageClass('Images/Får.jfif',undefined,undefined,200,3)
 // Et array der indholder vores sprits
-let animalSprits=['Images/cow.jfif','Images/Får.jfif','Images/gris.jfif'];
-let imgXpos=150;
-let imgYpos=300;
-let imgSize=150;
+let animalSprits=[cow.Sprites,pig.Sprites,sheep.Sprites];
+let imgXpos=cow.Xpos;
+let imgYpos=cow.Ypos;
+let imgSize=cow.size;
+let imgXposO=(window.innerWidth/3.7);
 let isGameInProgress=false;
 let PlayButton;
 let end =0;
+
+
 // en funktion der loader alle billederne 
 function preload() {
   for(let i=0; i <animalSprits.length; i ++){
@@ -18,21 +25,48 @@ function setup(){
   let canvas = createCanvas(window.innerWidth-18,window.innerHeight-18);
   background('lightgrey');
   PlayButton = createImg('/Images/playIcon.png.crdownload',"");
-  PlayButton.position(window.innerWidth/2,window.innerHeight/2);
-  PlayButton.mousePressed(drawImages(true));
+  PlayButton.position(window.innerWidth/2.05,window.innerHeight/4);
   PlayButton.size(50,50);
+  console.log("Spil tilstand " + isGameInProgress);
 };
-function drawImages(inp) {
-  if(isGameInProgress = inp){
+function drawImages() {
+  if(isGameInProgress == true){
     for(let n=0; n <animalSprits.length; n++){
        if(end < 3){
-       image(animalSprits[n],imgXpos,imgYpos,imgSize);
-       imgXpos += 200;}
+       image(animalSprits[n],imgXpos,imgYpos,imgSize,imgSize);
+       imgXpos += 250;}
        end ++}
      };
 };
+function mouseClicked() {
+  if (mouseX < ((window.innerWidth/2.05)+50) && mouseX >(window.innerWidth/2.05) && 
+  mouseY<((window.innerHeight/4)+50) &&mouseY>(window.innerHeight/4)){
+   isGameInProgress =true;
+   drawImages()
+   console.log("Spil tilstand " + isGameInProgress);
+  };
+ if(mouseX>imgXposO && mouseX<(imgXposO+200) && mouseY>imgYpos && 
+ mouseY<(imgYpos+200)){
+console.log(cow.id);
+ };
 
-// function draw(){
-//   drawImages();
+ if((mouseX>imgXposO+250) && mouseX<(imgXposO+500) && mouseY>imgYpos && 
+ mouseY<(imgYpos+200)){
+  console.log(pig.id);
+ };
+
+ if((mouseX>imgXposO+500) && mouseX<(imgXposO+750) && mouseY>imgYpos && 
+ mouseY<(imgYpos+200)){
+  console.log(sheep.id);
+ };
   
-// }
+};
+ function draw(){
+  
+  //  fill('')
+  //  rect(imgXposO,(window.innerHeight/1.8),200,200);
+  //  fill('red')
+  //  rect((imgXposO+250),(window.innerHeight/1.8),200,200);
+  //  fill('red')
+  //  rect((imgXposO+500),(window.innerHeight/1.8),200,200);
+};
